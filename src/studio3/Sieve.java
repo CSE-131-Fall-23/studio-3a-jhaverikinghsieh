156978429d91669d23b9c89fcbc068bbc23ec3da
CookieMusicStar <daphanie168@gmail.com>
2023-09-20 14:17:25 -0500

    Studio 3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,49 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+
+		// prompt user for a number
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("What is the maximum number? ");
+		int n = in.nextInt();
+		
+		// array of numbers
+		
+		boolean[] numbers = new boolean[n];
+		int[] prime = new int[50];
+		
+		// look for prime numbers
+		
+		for (int i = 0; i < n; i++) {
+			
+			numbers[i] = true;
+			
+			System.out.println(numbers[i]);
+			
+		}
+		
+		for (int j = 0; j < n; j++) {
+		
+			if () {
+				
+				for (int k = 2; k <= n; j++) {
+					
+					
+				}
+				
+			} else {
+				
+				
+				
+			}
+			
+		}
+			
+	}
+
+}
